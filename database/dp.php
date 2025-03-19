88907a191444de2a16549dfd0a973d46c3a80ddc
Metin <klaymanier@gmail.com>
2025-03-19 19:58:31 +0300

    SQL dosyasi olusturma ve veritabani baglantisini yapma

---
+++ database/dp.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$dbname = "giyim_sitesi";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    die("Bağlantı hatası: " . $e->getMessage());
+}
+?>
